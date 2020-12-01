38f72c305df9117d9de8fce905b39b5c7081f09b
Gwinnervy <69759605+Gwinnervy@users.noreply.github.com>
2020-12-01 19:27:58 +0700

    Add files via upload

---
+++ PTIT014..cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main ()
+{
+	int n;
+	cin >> n;
+	int k = 1;
+	for (int i = 0; i < n; ++i)
+	{
+		k = 2*k;
+		k = (k%10)%10; 
+	}
+	cout << k;
+	return 0;
+}
---
+++ PTIT023..cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main ()
+{
+	int n;
+	cin >> n;
+	cout << 2021-n;
+	return 0;
+}
