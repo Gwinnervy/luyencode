672a55e3a9efafe9adaf067ed2348c324a6eecc5
Thanh Dat Le <JamesGrantEllison@live.com>
2020-12-05 04:07:51 +0000

    Update PTIT012.cpp

--- PTIT012.cpp
+++ PTIT012.cpp
@@ -1,12 +1,53 @@
-#include <bits/stdc++.h>
-#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-using namespace std;
+int convert_str_to_int(std::string &str) {
+  int val;
+  try {
+    val = std::stoi(str);
+  } catch (std::invalid_argument &) {
+  } catch (std::out_of_range &) {
+  }
 
-int main ()
-{
-	long long a,b;
-	cin >> a >> b;
-	cout << a+b << " " << a-b << " " << a*b;
-	return 0;
+  return val;
+}
+
+std::vector<std::string> split_string(std::string &str) {
+  char delim = ' ';
+  std::string token;
+  std::stringstream ss(str);
+  std::vector<std::string> vect = {};
+
+  while (std::getline(ss, token, delim)) {
+    vect.push_back(token);
+  }
+
+  return vect;
+}
+
+int add(int &a, int &b) { return a + b; }
+
+int subtract(int &a, int &b) { return a - b; }
+
+int multiply(int &a, int &b) { return a * b; }
+
+int main(int argc, char *argv[]) {
+  int a;
+  int b;
+  std::string str;
+  std::vector<std::string> vect;
+
+  std::getline(std::cin, str);
+  vect = split_string(str);
+
+  a = convert_str_to_int(vect.at(0));
+  b = convert_str_to_int(vect.at(1));
+
+  std::cout << add(a, b) << ' ';
+  std::cout << subtract(a, b) << ' ';
+  std::cout << multiply(a, b) << '\n';
+
+  return 0;
 }
